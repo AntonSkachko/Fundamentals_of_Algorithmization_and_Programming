5afeb7eb7f71925d38444a90c19267b30cda438c
Anton Skachko <63850442+Ant0n228@users.noreply.github.com>
2021-11-27 00:13:30 +0300

    laba_5_second_level

---
+++ laba_5/second_level.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int n, m;
+	cout << "Enter n: "; cin >> n;
+	cout << "Enter m: "; cin >> m;
+
+	int** matr = new int* [n];
+
+	for (int i = 0; i < n; i++) {
+		matr[i] = new int[m];
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> matr[i][j];
+		}
+	}
+
+	int* temp;
+	for (int i = 0; i + 1 < m; ++i) {
+		for (int j = 0; j < m - i - 1; ++j) {
+			if (matr[j][0] > matr[j + 1][0]) {
+				temp = matr[j];
+				matr[j] = matr[j + 1];
+				matr[j + 1] = temp;
+			}
+		}
+	}
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < m; ++j) {
+			cout << matr[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	system("pause");
+	for (int i = 0; i < n; i++) {
+		delete[] matr[i];
+	}
+	delete[] matr;
+	return 0;
+}
