59a62897b3682bac204db16225bac54985c28364
Anton Skachko <63850442+Ant0n228@users.noreply.github.com>
2021-11-26 20:46:58 +0300

    laba_1_first_level

---
+++ laba_1/first_level.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+const double PI = acos(-1);
+
+int main() {
+	double z1, z2; // result
+	double a, b; // variables
+	cout << "Enter a: "; cin >> a;
+	cout << "Enter b: "; cin >> b;
+
+	double x, y; // additional variables
+	
+	x = 2 * pow(sin(3 * PI - 2 * a), 2);
+	y = pow(cos(5 * PI + 2 * a), 2);
+
+	z1 = x * y;
+	cout << "Result 1: " << z1 << endl;
+
+	z2 = 1 / .4 - 1 / .4 * sin(5 / .2 * PI - 8 * a);
+	cout << "Result 2: " << z2 << endl;
+
+	return 0;
+}
