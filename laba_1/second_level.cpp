150d42deecbd019040895d0443887de19b9d1ac7
Anton Skachko <63850442+Ant0n228@users.noreply.github.com>
2021-11-26 20:54:44 +0300

    laba_1_second_laba

---
+++ laba_1/second_level.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	double a, b; // variables
+	cout << "Enter a: "; cin >> a;
+	cout << "Enter b: "; cin >> b;
+
+	double x, y; // additional variables
+	
+	x = (a - 1) * sqrt(a) - (b - 1) * sqrt(b);
+	y = sqrt(a * a * a * b) + a * b + a * a - a;
+
+	cout << "First result: " << x / y << endl;
+	cout << "Second result: " << (sqrt(a) - sqrt(b)) / a;
+	return 0;
+}
