61831072e63ac3c92ced1787b17758cbce831230
Anton Skachko <63850442+Ant0n228@users.noreply.github.com>
2021-11-26 21:15:28 +0300

    laba_4

---
+++ laba_4/first_level.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int n, average = 0;
+	cout << "n: ";  cin >> n;
+	int* arr = new int[n];
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	for (int i = 0; i < n; i++) {
+		if (arr[i] > 0) {
+			average += arr[i];
+		}
+	}
+	cout << "average: " << average << endl;
+
+
+	int k, max_ = 0, index_max = 0, multiplication = 1;
+	cout << "k: "; cin >> k;
+	for (int i = 0; i < k; i++) {
+		cin >> arr[i];
+	}
+
+	for (int i = 0; i < k; i++) {
+		if (arr[i] > max_) {
+			max_ = arr[i];
+			index_max = i;
+		}
+	}
+
+	for (int i = index_max; i < k; i++) {
+		multiplication *= arr[i];
+	}
+	cout <<"multiplication: " << multiplication;
+
+	delete[]arr;
+	return 0;
+}
---
+++ laba_4/second_level.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <ctime>
+#include <random>
+using namespace std;
+
+int main() {
+	srand(time(0));
+	int n, choice, index_x = -1, index_y = -1, sum = 0;
+	cout << "n: "; cin >> n;
+	int* arr = new int[n];
+
+	cout << "How do you want to work your programm" << endl;
+	cout << "Random: Enter 1" << endl;
+	cout << "Enter in keyboard: Enter 2" << endl;
+
+	cin >> choice;
+
+	switch (choice) {
+		case 1:
+			for (int i = 0; i < n; i++) {
+				arr[i] = 1 + rand() % 10;
+			}
+			break;
+		case 2:
+			for (int i = 0; i < n; i++){
+				cout << "Enter " << i << " elemet: ";
+				cin >> arr[i];
+			}
+
+	}
+	
+	for (int i = 0; i < n; i++) {
+		if (arr[i] > 0) {
+			index_x = i;
+			break;
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		if (arr[i] < 0) {
+			index_y = i;
+			break;
+		}
+	}
+
+	for (int i = index_x; i < index_y; i++) {
+		sum += arr[i];
+	}
+
+	cout << sum;
+
+	delete[]arr;
+	return 0;
+}
