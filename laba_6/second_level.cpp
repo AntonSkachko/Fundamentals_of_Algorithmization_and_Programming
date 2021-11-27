33cfafd4d04ed7ad68a4ae6a01e9419277087634
Anton Skachko <63850442+Ant0n228@users.noreply.github.com>
2021-11-27 23:14:32 +0300

    6_laba_second_level

---
+++ laba_6/second_level.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string s1, s2;
+	cout << "Enter string: "; cin >> s1;
+	for (auto c : s1) {
+		if (c >= '0' && c <= '9') {
+			s2 += c;
+		}
+		if(c == '.' && s2[s2.size()- 1] >= '0'
+			&& s2[s2.size() - 1] <= '9') {
+			s2 += c;
+		}
+		if (c == 'e' && s2[s2.size() - 1] >= '0'
+			&& s2[s2.size() - 1] <= '9') {
+			s2 += c;
+		}
+		if (c == '-' && s2[s2.size() - 1] >= 'e') {
+			s2 += "-";
+		}
+		if (c == '+' && s2[s2.size() - 1] >= 'e') {
+			s2 += '+';;
+		}
+	}
+	cout << "Real floating point number: " << s2;
+	return 0;
+}
