cf8e0c2ef5e2aa7576cd6eecc92d5e2e9cf29bae
Anton Skachko <63850442+Ant0n228@users.noreply.github.com>
2021-12-06 18:42:52 +0300

    third_level

---
+++ variant13/laba_2/third_level.cpp
@@ -0,0 +1,27 @@
+/**
+      ---------------------------------------------------
+      |         Made by Shamal Viktor (@j3uktop)        |
+      |           2019-2021 - Lyceum BSU - IM           |
+      |             2021 - BSUIR - FCP - PMS            |
+      |          *** All rights reserved ***            |
+      ---------------------------------------------------
+                                            			**/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    double x, y, answ;
+    cout << "x = ";     cin >> x;
+    cout << "y = ";     cin >> y;
+
+    if (abs(x) + abs(y) <= 1 || x >= 0)        answ = max(x, y) + sqrt(x);
+    else if (abs(x) + abs(y) > 0 || (x < 0 && y < 0)) answ = min(x, y) - sin(x) * sin(x) - cos(y) * cos(y);
+        else answ = exp(x * x + abs(y));
+
+
+    cout << "Result = " << answ << '\n';
+    system("pause");
+    return 0;
+}
