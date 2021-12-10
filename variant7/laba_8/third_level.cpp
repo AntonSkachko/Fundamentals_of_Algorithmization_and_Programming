a5bf95e147328e4bc5b56cd30cfd19043419033a
Anton Skachko <63850442+Ant0n228@users.noreply.github.com>
2021-12-10 14:04:32 +0300

    Create third_level.cpp

---
+++ variant7/laba_8/third_level.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+double getSqrt(const double& a, int n) {
+    if (n == 0) {
+        return 0.5 * (1 + a);
+    }
+    double buf = getSqrt(a, n - 1);
+    return 0.5 * (buf + a / buf);
+}
+
+int main()
+{
+    double a, n;
+    cout << "Enter a = ";   cin >> a;
+    cout << "Enter n = ";   cin >> n;
+    cout << getSqrt(a, n) << endl;
+    return 0;
+}
