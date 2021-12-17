9db932754cefc832b5a70fc92db99a8cb9686e0c
Anton Skachko <63850442+Ant0n228@users.noreply.github.com>
2021-12-17 19:35:22 +0300

    first_level_laba2

---
+++ variant10/laba_2/first_level.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	double x, y, z, m;
+	cout << "x = "; cin >> x ;
+	cout << "y = "; cin >> y ;
+	cout << "z = "; cin >> z ; 
+	m = max(max(x, y), max(y, z)) / min((x, y), z);
+	cout << "   result = " << m << endl;
+	return 0;
+
+}
+
