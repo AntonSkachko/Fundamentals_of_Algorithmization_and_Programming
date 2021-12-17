f5122f984d7056451e2dbf23c68b5d58251d00a8
Anton Skachko <63850442+Ant0n228@users.noreply.github.com>
2021-12-17 19:41:17 +0300

    Create second_level.cpp

---
+++ variant10/laba_8/second_level.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+double sqrtPlus(int n, int a = 1) {
+    if (a == n) {
+        return sqrt(n);
+    }
+    return sqrt(a + sqrtPlus(n, a + 1));
+}
+int main()
+{
+    double n;
+    cout << "n = ";   cin >> n;
+    cout << sqrtPlus(n);
+    return 0;
+}
