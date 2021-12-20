85de55e48eb1e6ea8fce139755f22f4ab98a1835
Anton Skachko <63850442+Ant0n228@users.noreply.github.com>
2021-12-20 21:29:20 +0300

    third_level

---
+++ variant11/laba_4/third_level.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int number;
+	cout << "Enter n:";
+	cin >> number;
+
+ 	if (number == 0) {
+		cout << "Your number is 0";
+		return 0;
+	}
+
+	int index = 0, num = number;
+	while (number > 0) {
+		index++;
+		number /= 10;
+	}
+	int* array = new int[index];
+
+	for (int i = 0; i < index; i++) {
+		array[i] = pow(num % 10, 2);
+		num /= 10;
+	}
+
+	cout << "Result: ";
+	for (int i = index - 1; i > -1; i--) {
+		cout  << array[i] << " ";
+	}
+
+	return 0;
+
+}
