02910d3953ef8d91ecf744cab3fba35c6dfdfb0d
Anton Skachko <63850442+Ant0n228@users.noreply.github.com>
2021-12-20 20:21:40 +0300

    first_level_laba4

---
+++ variant11/laba_4/first_level.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int length;
+    cout << "Enter length of array: ";
+    cin >> length;
+    int* array = new int[length];
+
+    for (int i = 0; i < length; i++)
+    {
+        cout << "Enter array[" << i << "]: ";
+        cin >> array[i];
+    }
+
+    int min = array[0], minIndex = 0;
+    for (int i = 1; i < length; i++)
+    {
+        if (min > array[i])
+        {
+            min = array[i];
+            minIndex = i;
+        }
+    }
+
+    int sum = 0;
+    for (int i = minIndex + 1; i < length; i++)
+    {
+        sum += array[i];
+    }
+
+    cout << "Sum = " << sum << endl;
+    return 0;
+}
