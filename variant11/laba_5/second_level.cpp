5907f744fbd5ee9ff0b3d0df17d20c020f5e9fe5
Anton Skachko <63850442+Ant0n228@users.noreply.github.com>
2021-12-21 17:30:22 +0300

    nedo

---
+++ variant11/laba_5/second_level.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int n, m;
+	cout << "Enter n: "; cin >> n;
+	cout << "Enter m: "; cin >> m;
+
+	int** matr = new int* [n];
+
+	for (int i = 0; i < n; i++) {
+		matr[i] = new int[m];
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << "matr[" << i << "][" << j << "] = ";
+			cin >> matr[i][j];
+		}
+	}
+
+	int count = 0;
+	for (int j = 0; j < m; j++) {
+		for (int i = 0; i < n; i++) {
+			if (matr[i][j] < matr[i + 1][j] && matr[i][j] > matr[i - 1][j]) {
+				if (i != 0 && i != n){
+					count++;
+				}
+			}
+		}
+	}
+
+	cout << "Resutl: " << count;
+	return 0; 
+}
