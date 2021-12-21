e75f989efde295d53a5d0aa183695675cbe3d41a
Anton Skachko <63850442+Ant0n228@users.noreply.github.com>
2021-12-21 16:31:39 +0300

    first_level

---
+++ variant11/laba_5/first_level.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int n, m;
+	cout << "Enter n: "; cin >> n;
+	cout << "Enter m: "; cin >> m;
+	int** matr = new int*[n];
+
+	for (int i = 0; i < n; i++) {
+		matr[i] = new int[m];
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << "matr[" << i << "][" << j << "] = ";
+			cin >> matr[i][j];
+		}
+	}
+
+	int k;
+	cout << "Enter k: "; cin >> k;
+
+	int count = 0, sum = 0;
+	for (int j = 0; j < m; j++) {
+		for (int i = 0; i < n; i++) {
+			sum += matr[i][j
+			];
+		}
+		if (sum < k) {
+			count++;
+		}
+	}
+
+	cout << "Result: " << count;
+}
