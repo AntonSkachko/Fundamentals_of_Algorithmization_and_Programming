700c4836e48f0cf08f806b2ccd1085062a01c07f
Anton Skachko <63850442+Ant0n228@users.noreply.github.com>
2021-12-26 14:48:40 +0300

    first_level

---
+++ variant1/laba_1/first_level.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+const double PI = acos(-1);
+
+int main() {
+	int z1, z2, a, b, x, y;
+	cout << "Enter a: "; cin >> a;
+	cout << "Enter b: "; cin >> b;
+
+	x = pow(cos(5 * PI + 2 * a), 2) * 2;
+	z1 = pow(sin(3 * PI - 2 * a), 2) * x;
+
+
+	z2 = 1. / 4 - 1. / 4 * sin(5. / 2 * PI - 8 * a);
+
+	cout << "Result 1: " << z1 << endl;
+	cout << "Result 2: " << z2 << endl;
+
+	return 0;
+
+}
