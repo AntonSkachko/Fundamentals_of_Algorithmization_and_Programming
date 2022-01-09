5bfb724abd0841f84f2d620cefc2e4d8f916011b
Anton Skachko <63850442+Ant0n228@users.noreply.github.com>
2022-01-09 13:41:51 +0300

    5_laba

---
+++ variant4/laba_5/second_level.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+using namespace std;
+int main() {
+
+	cout << "Whrite size of matrix" << endl;
+
+	int n,m,i,j,count = 0, sum = 0;    
+	cout << "Number of rows = ";     cin >> n;
+	cout << "Number of columns = ";  cin >> m;
+	
+	int** a = new int* [n];
+
+	for ( i = 0; i < n; i++) 
+	{
+		a[i] = new int[m];
+		for (j = 0; j < m; j++) 
+		{
+			cout << "array[" << i << "][" << j << "] = "; cin >> a[i][j];
+		}
+	}
+
+	cout << "\nMatrix:\n\n";
+	for ( i = 0; i < n; i++) 
+	{
+		for ( j = 0; j < m; j++)
+			cout<< a[i][j]<< "  ";
+		cout << endl;
+	}
+	for ( i = 0; i < n; i++)	
+		for ( j = 0; j < m; j++)
+		{
+			for (int t = 0; t < n; t++) sum += a[t][j];
+			sum -= a[i][j];
+			if (sum < a[i][j]) count++;
+			sum = 0;
+		}	
+	cout << "The result = " << count << endl;
+	for ( i = 0; i < n; i++)
+		delete[] a[i];
+	delete[] a;
+	system("pause");
+	return 0;
+}
---
+++ variant4/laba_5/third_level.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	int size,i,j;
+	cout << "Enter size of matrix : "; cin >> size;
+	int** a = new int* [size];
+	for ( i = 0; i < size; i++)
+	{
+		a[i] = new int[size];
+		for ( j = 0; j < size; j++) {
+			cout << "Matrix [" << i << "] [" << j << "] = "; cin >> a[i][j];
+		}
+	}
+	cout << "Matrix :\n";
+	for ( i = 0; i < size; i++)
+	{
+		for ( j = 0; j < size; j++) cout << " " << a[i][j];
+
+		cout << endl;
+	}
+	cout << endl;
+	cout << "New matrix :\n";
+	for ( i = 0; i < size; i++) {
+		for ( j = 0; j < size; j++) 	cout << " " << a[size - j - 1][i];
+		cout << endl;
+	}
+
+	for ( i = 0; i < size; i++)
+		delete[] a[i];
+	delete[] a;
+	return 0;
+}
