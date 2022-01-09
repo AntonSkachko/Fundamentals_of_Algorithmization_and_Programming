d758b57efb5f03f2df5aaf1cc57f48f6557c2638
Anton Skachko <63850442+Ant0n228@users.noreply.github.com>
2022-01-09 13:43:31 +0300

    8 laba

---
+++ variant4/laba_8/third_level.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+using namespace std;
+int fib(int n)       //функция без рекурсии
+{
+	if (n == 0) return 0;
+	int a = 1, b = 0, k;
+	for (int i = 1; i <= n; i++) 
+	{
+		k = b + a;
+		a = b;
+		b = k;
+	}
+	return k;
+}
+int f(int n)  // рекурсивная функция
+{
+	if (n <2) return n;
+	return f(n - 1) + f(n - 2);
+}
+int main()
+{
+	int n;
+	cout << "n = ";  cin >> n;
+	cout << "Fb1(" << n << ") = " << fib(n) << endl;
+	cout << "Fb2(" << n << ") = " << f(n) << endl;
+	return 0;
+}
