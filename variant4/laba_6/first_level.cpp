5df51aa9f01f6cff65a4eb817d7bf17a76f32156
Anton Skachko <63850442+Ant0n228@users.noreply.github.com>
2022-01-09 13:43:00 +0300

    6_laba

---
+++ variant4/laba_6/first_level.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+	bool flag = true;
+	string text; 
+	char a;
+	cout << "Write content of line: "; getline(cin, text);
+	cout << "Enter symbol : ";    cin >> a;
+	for (int i = 0; i < text.size(); i++) 
+		if (text[i] == a) 
+		{
+			text = text.substr(0, i + 1) + a + text.substr(i + 1, text.size() - i - 1);
+			i++;
+			flag = false;
+		}
+	if (!flag) cout << text << endl;
+	else cout << "There is no symbol " << a;
+	return 0;
+}
---
+++ variant4/laba_6/second_level.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+    string s;
+    cout << "Enter string : "; cin >> s;
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        int k = i;
+        while (s[i] == s[i + 1]) i++;
+        if ((abs(k - i) % 2 == 1))
+        {
+            for (int t = k; t <= i; t++)
+                cout << s[t];
+            cout << endl;
+        }
+    }
+    system("pause");
+    return 0;
+}
