f0da64dc1d574b73a18d695a416b3d77955f6d38
Anton Skachko <63850442+Ant0n228@users.noreply.github.com>
2022-01-28 17:22:52 +0300

    so bad

---
+++ variant1/laba_5/first_level.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int line, column;
+	cout << "Enter line: "; cin >> line;
+	cout << "Enter column: "; cin >> column;
+
+	int** matr = new int* [line];
+	for (int i = 0; i < line; i++) {
+		matr[i] = new int[column];
+	}
+
+	for (int i = 0; i < line; i++) {
+		for (int j = 0; j < column; j++) {
+			cout << "Matr[" << i << "][" << j << "]: "; 
+			cin >> matr[i][j];
+		}
+	}
+
+	int* yourArray = new int[column];
+
+	for (int i = 0; i < column; i++) {
+		yourArray[i] = 0;
+	}
+
+	for (int i = 0; i < line; i++) {
+		for (int j = 0; j < column; j++) {
+			if (matr[i][j] == 0) {
+				yourArray[j]++;
+			}
+		}
+	}
+	int xyq = 0;
+	for (int i = 0; i < column; i++) {
+		if (yourArray[i] != 0) {
+			xyq++;
+		}
+	}
+	cout << xyq;
+	delete yourArray;
+	for (int i = 0; i < line; i++) {
+		delete[] matr[column];
+	}
+	delete[] matr;
+	return 0;
+}
