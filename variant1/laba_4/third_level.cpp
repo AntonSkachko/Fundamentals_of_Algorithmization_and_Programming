a3be50c4cc49721ecb02cec3ab6d142c6d7c7cf1
Anton Skachko <63850442+Ant0n228@users.noreply.github.com>
2022-01-28 14:56:21 +0300

    third_level

---
+++ variant1/laba_4/third_level.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int sizeArray(int number) {
+	int amountOfNumber = 0;
+	int newNum;
+	while (number) {
+		newNum = number % 10;
+		if (newNum % 3 == 0) {
+			amountOfNumber++;
+		}
+		number /= 10;
+	}
+	return amountOfNumber;
+	
+}
+void seachMultiplesOfThree(int amountOfNumber, int number) {
+	int* yourArray = new int[amountOfNumber];
+	int newNum;
+	int i = 0;
+	while(number) {
+		newNum = number % 10;
+		if (newNum % 3 == 0) {
+			yourArray[i] = number % 10;
+			i++;
+		}
+		
+		number /= 10;
+	}
+	for (int i = 0; i < amountOfNumber; i++) {
+		cout << yourArray[i] << " ";
+	}
+	delete[] yourArray;
+
+}
+
+
+int main() {
+	int number;
+	cout << "Enter number "; cin >> number;
+	
+	int amountOfNumber = sizeArray(number);
+	
+	if (amountOfNumber == 0) {
+		cout << "This number don't have number % 3!!!";
+		return 0;
+	}
+
+	cout << amountOfNumber << endl;
+	seachMultiplesOfThree(amountOfNumber, number);
+	
+
+	return 0; 
+
+}
