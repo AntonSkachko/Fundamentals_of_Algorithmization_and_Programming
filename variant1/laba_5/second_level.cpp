b98da6d1a627a373822fa8cab2aab8cfe13bb9cc
Anton Skachko <63850442+Ant0n228@users.noreply.github.com>
2022-01-29 16:46:35 +0300

    second_level

---
+++ variant1/laba_5/second_level.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+using namespace std;
+
+int** createMatrix(int line, int column);
+void createVector(int line, int** matrix, int column);
+
+int main() {
+	int line, column;
+	cout << "Enter line: "; cin >> line;
+	cout << "Enter column: "; cin >> column;
+	
+	int** matrix = createMatrix(line, column);
+
+	for (int i = 0; i < line; i++) {
+		for (int j = 0; j < column; j++) {
+			cout << matrix[i][j] << " ";
+		}
+		cout << endl;
+	}
+	
+	createVector(line, matrix, column);
+	
+	for (int i = 0; i < line; i++) {
+		delete[]matrix[i];
+	}
+	delete[]matrix;
+
+	return 0;
+}
+
+int** createMatrix(int line, int column) {
+	int** matr = new int* [line];
+
+	for (int i = 0; i < line; i++) {
+		matr[i] = new int[column];
+	}
+
+	for (int i = 0; i < line; i++) {
+		for (int j = 0; j < column; j++) {
+			cout << "matr[" << i << "][" << j << "]: ";
+			cin >> matr[i][j];
+		}
+	}
+	return matr;
+}
+
+void createVector(int line, int**matrix, int column) {
+	int* myVector = new int[column];
+	for (int i = 0; i < line; i++) {
+		int buff = 1;
+		for (int j = 1; j < column; j++) {
+			if (matrix[i][j] > matrix[i][j - 1]) {
+				buff++;
+			}
+		}
+		if (buff  == column) {
+			myVector[i] = 1;
+		}
+		else {
+			myVector[i] = 0;
+		}
+	}
+	
+	for (int i = 0; i < column; i++) {
+		cout << myVector[i] << " ";
+	}
+}
